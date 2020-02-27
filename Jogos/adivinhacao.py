a7c7dc983ab638dbb032330f166b35bae271a02a
Batatinha <batatinha@batatinha.meuintelbras.local>
2020-02-26 23:11:39 -0300

    Aula 6 - Numero aleatorio, e níveis

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -1,14 +1,36 @@
-print("**************************************")
-print("* Bem vindo ao jogo de adivinhação!! *")
-print("**************************************")
+import random
 
-numero_secreto = 42
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
 
-chute = int(input("Digite o número secreto: "))
+numero_secreto = random.randrange(1, 101)
+print("Escolha o nível:")
+print("1 - Fácil / 2 - Médio / 3 - Difícil")
+nivel = int(input("Nível desejado: "))
+total_de_tentativas = int(18/nivel)
 
-print("Você digitou ", chute)
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um número entre 1 e 100: ")
+    chute = int(chute_str)
 
-if chute == numero_secreto:
-    print("Você acertou!")
-else:
-    print("Você errou")
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if(acertou):
+        print("Você acertou!")
+        break
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+
+print("o número secreto era", numero_secreto)
+print("Fim do jogo")
