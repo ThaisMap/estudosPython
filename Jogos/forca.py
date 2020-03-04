9c6c745e22039a98f55420e677d2cb1b583ccd50
ThaisMap <thais.marpi@gmail.com>
2020-03-03 23:38:06 -0300

    Inicio curso 2

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -3,6 +3,21 @@ def jogar():
     print("* Bem vindo ao jogo de Forca! *")
     print("*******************************")
 
+    palavra_secreta = "cachorro"
+
+    acertou = False
+    enforcou = False
+
+    while(not acertou and not enforcou):
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra na posicao {}".format(index))
+            index = index+1
+        print("Jogando...")
+
     print("Fim do jogo")
 
 
