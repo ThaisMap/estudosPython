5d7a221002d48b5dd2521dcc961392984aa78bfd
ThaisMap <thais.marpi@gmail.com>
2020-03-05 22:41:56 -0300

    Curso 2 - Aula 6

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,24 +1,47 @@
+import random
 def jogar():
     print("*******************************")
     print("* Bem vindo ao jogo de Forca! *")
     print("*******************************")
 
-    palavra_secreta = "cachorro"
+    palavras = []
+    with open("palavras.txt") as arquivo:
+        for linha in arquivo:
+            palavras.append(linha.strip())
+
+    palavra_secreta = palavras[random.randrange(0, len(palavras))]
+
+    palavra_secreta = palavra_secreta.upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     acertou = False
     enforcou = False
 
+    erros = 0
+    print(letras_acertadas)
+
     while(not acertou and not enforcou):
         chute = input("Qual a letra? ")
-        chute = chute.strip()
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra na posicao {}".format(index))
-            index = index+1
-        print("Jogando...")
+        chute = chute.strip().upper()
+        if chute not in palavra_secreta:
+            erros += 1
+        else:
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index = index+1
+
+        print(letras_acertadas)
+
+        acertou = "_" not in letras_acertadas
+        enforcou = erros == 6
 
     print("Fim do jogo")
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você enforcou")
 
 
 if __name__ == "__main__":
